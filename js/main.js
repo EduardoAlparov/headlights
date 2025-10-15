c31788a17dcb7e7490d01da8a136b3f027cf4f58
EduardoAlparov <workman133@yandex.ru>
2025-10-15 20:16:16 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const body = document.body;
+
+    // activate transition:
+    setTimeout(() => {
+        body.classList.remove('preload');
+    }, 500);
+
+})
