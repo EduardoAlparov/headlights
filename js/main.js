582225f2e4b2d834dad5d1f27b16f37dd40c6c7c
EduardoAlparov <workman133@yandex.ru>
2025-10-17 00:55:14 +0300

    dev: work orders ready 16.10.25

--- js/main.js
+++ js/main.js
@@ -6,4 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
         body.classList.remove('preload');
     }, 500);
 
+    const itemDropdonwBtns = document.querySelectorAll('.js-item-dropdown-btn');
+
+    itemDropdonwBtns.forEach( (btn) => {
+        btn.addEventListener('click', (e) => {
+            btn.classList.toggle('js-dropdown-btn--active');
+
+            const select = e.target.closest('.b-select');
+
+            if(!select) return;
+
+            select.querySelector('.b-select__close').addEventListener('click', () => {
+                btn.classList.remove('js-dropdown-btn--active');
+            })
+        })
+    });
+
 })
